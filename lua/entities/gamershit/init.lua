2bdbd64cf387a6dc997862e8391a75cae3965870
= <damierny@gmail.com>
2019-08-10 16:52:21 -0500

    yes

--- lua/entities/gamershit/init.lua
+++ lua/entities/gamershit/init.lua
@@ -1,1 +1,46 @@
-c
+ENT.PrintName = "Spero Shit Tosser"
+ENT.RenderGroup = RENDERGROUP_BOTH
+
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+
+AddCSLuaFile( "cl_init.lua" ) -- Make sure clientside
+AddCSLuaFile( "shared.lua" )  -- and shared scripts are sent.
+
+include('shared.lua')
+
+
+if SERVER then
+    function ENT:Initialize()
+        self:Think()
+    end
+    function ENT:Explode()
+        local Damage = 1000
+        local BlastRadius = 250
+
+        local EntsInsideTheBoomBoomZone = ent.FindInSphere(self:GetPos(),BlastRadius)
+
+        for Index, Value in pairs(EntsInsideTheBoomBoomZone) do
+            local Ratio = 1 - (self:GetPos():Distance(Value:GetPos()) / BlastRadius)
+            Value:TakeDamage(Damage * Ratio,self["owner"],self) --damwhat * whatmydude
+        end
+
+    end
+    function ENT:Think() --https://wiki.garrysmod.com/page/util/TraceLine
+        local velocity = self["velocity"]
+        local BulletTrace = util.TraceLine({
+            start = self:GetPos(),
+            endpos = self:GetPos() + velocity
+        })
+
+        self:SetPos(BulletTrace.Hitpos)
+        self:SetAngle(velocity:Angle())
+
+        if IsValid(BulletTrace.Entity) or BulletTrace.HitWorld then
+                self:Explode()
+        end
+    end
+end
+-- VALUES IN THE ENTITY
+-- owner
+-- velocity
--- lua/weapons/gamercuminajar.lua
+++ lua/weapons/gamercuminajar.lua
@@ -1,1 +1,55 @@
-b
+
+
+
+SWEP.Weight				= 5
+SWEP.AutoSwitchTo		= false
+SWEP.AutoSwitchFrom		= false
+SWEP.AdminSpawnable			= false
+SWEP.Author					= "Spero and CoSperoAtors"
+SWEP.Contact				= ""
+
+SWEP.Instructions			= "yotE"
+
+SWEP.Primary.Ammo			= "pistol"
+SWEP.Primary.Automatic		= false
+SWEP.Primary.ClipSize		= 1
+SWEP.Primary.DefaultClip	= 1
+SWEP.Purpose				= "splatter those tatters"
+SWEP.Secondary.Ammo			= "none"
+SWEP.Secondary.Automatic	= true
+SWEP.Secondary.ClipSize		= -1
+SWEP.Secondary.DefaultClip	= -1
+SWEP.Spawnable				= true
+SWEP.ViewModelFlip			= false
+SWEP.ViewModelFOV			= 55
+SWEP.ViewModel				= "models/weapons/v_pistol.mdl"
+SWEP.WorldModel				= "models/weapons/w_pistol.mdl"
+
+SWEP.PrintName			= "RPSpero"
+SWEP.Slot				= 0
+SWEP.SlotPos			= 5
+SWEP.IconLetter			= "G"
+SWEP.DrawAmmo			= true
+SWEP.DrawCrosshair		= true
+
+
+
+
+
+function SWEP:Initialize()
+	if SERVER then 
+		self:SetWeaponHoldType("pistol")
+	end
+end
+
+if SERVER then
+	function SWEP:PrimaryAttack()
+        local speed = 10
+        local entity = ents.Create("gamershit")
+        entity:SetPos(SWEP:GetOwner():GetShootPos():Add(SWEP:GetOwner():EyeAngles():Forward()*100))
+        Bullet:SetModel("models/props_c17/oildrum001.mdl")
+        entity["owner"] = SWEP:GetOwner()
+        entity["velocity"] = SWEP:GetOwner():EyeAngles():Forward() * speed
+	end
+end
+
